460d030585865e6bcd553f160ebed28b2100a19d
menxs <diego.valcarcel.barbeito@gmail.com>
2021-05-11 20:11:37 +0200

    publish on hexpm

--- mix.exs
+++ mix.exs
@@ -32,6 +32,22 @@ defmodule MafiaEngine.MixProject do
     ]
   end
 
+  defp description() do
+    "A still under development package for the party game mafia."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "mafia_engine",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => ""}
+    ]
+  end
+
   defp dialyzer_opts do
     [
       plt_add_apps: [:ex_unit],
