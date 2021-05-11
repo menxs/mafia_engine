256ec9f439a0e204f3cc3355878d32c0a762ecf9
menxs <diego.valcarcel.barbeito@gmail.com>
2021-05-11 20:12:44 +0200

    publish on hexpm

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule MafiaEngine.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       dialyzer: dialyzer_opts()
     ]
